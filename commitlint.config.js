a6bdd8f8f6873a0a9b4ed034bd48e12638d3fed6
Pradyumna <pradyumna.j@idexcel.com>
2025-02-10 18:15:28 +0530

    [fix-123](dashboard): 12345678

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,38 +1,40 @@
 module.exports =  {
     parserPreset: {
         parserOpts: {
-            headerPattern: /^\[(\w+)-(\d+)\]\((\w+)\): (.+)$/g,
+            headerPattern: /^\[(.+?)-(\d+)\]\((.+?)\): (.+)$/,
             headerCorrespondence: ['type', 'ticketNumber', 'scope', 'description'],
         }
     },
     plugins : [{
         rules: {
-            'type-empty': (parsed) => {
-                const { type } = parsed;
-                console.log('type', type);
+            'type-empty': (parsed, _when) => {
+                console.log('parsed', parsed);
                 
-                if (!type) {
-                    return [false, 'type is required'];
-                }
+                const { type, ticketNumber, scope, description } = parsed;
+                console.log('type', type, "description", description, "ticketNumber", ticketNumber, "scope", scope);
+                
+                if (!type) return [false, 'type is required'];
                 return [true];
             },
-            'type-enum': (parsed, expectedValues) => {
+            'type-enum': (parsed, _when , expectedValues) => {
                 const { type } = parsed;
+                console.log('expectedValues', expectedValues);
+                
                 if (!type || !expectedValues.includes(type)) {
-                    return [false, `type must be one of ${expectedValues}`];
+                    return [false, `type must be one of ${expectedValues.join(', ')}`];
                 }
                 return [true];
             },
-            'description-min-length': (parsed, expectedValues) => {
+            'description-min-length': (parsed, _when, expectedValues) => {
                 const { description } = parsed;
-                if (!description || description.length < expectedValues[0]) {
+                if (!description || description?.length < expectedValues[0]) {
                     return [false, `description must be at least ${expectedValues[0]} characters, current length is ${description.length || 0}`];
                 }
                 return [true];
             },
-            'description-max-length': (parsed, expectedValues) => {
+            'description-max-length': (parsed, _when, expectedValues) => {
                 const { description } = parsed;
-                if (!description || description.length > expectedValues[0]) {
+                if (!description || description?.length > expectedValues[0]) {
                     return [false, `description must be less than ${expectedValues[0]} characters, current length is ${description.length || 0}`];
                 }
                 return [true];
@@ -40,7 +42,7 @@ module.exports =  {
         }
     }],
     rules: {
-        'type-empty': [2, 'never'],
+        'type-empty': [2, 'always'],
         'type-enum': [2, 'always', ['feat', 'fix', 'chore', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'revert']],
         'description-min-length': [2, 'always', 10],
         'description-max-length': [2, 'always', 100],
