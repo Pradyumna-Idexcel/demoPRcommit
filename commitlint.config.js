c558de424e8f06a7a090bee9062cd6e6365bff89
Pradyumna <pradyumna.j@idexcel.com>
2025-02-13 14:14:29 +0530

    ci: LOSDDDDDDF-61371 removed moduleName

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,16 +1,16 @@
 module.exports =  {
     parserPreset: {
         parserOpts: {
-            headerPattern: /^(\S+)?-(\S+)-(\S+)\((\S+)\): (\S(?:.*\S)?)$/,
-            headerCorrespondence: ['type','ticketKey', 'ticketNumber', 'moduleName', 'description'],
+            headerPattern: /^(\S+)?: (\S+)-(\S+) (\S(?:.*\S)?)$/,
+            headerCorrespondence: ['type','ticketKey', 'ticketNumber', 'description'],
         }
     },
     plugins : [{
         rules: {
             'header-format': (parsed, _when) => {
-                const headerPattern = /^(\S+)?-(\S+)-(\S+)\((\S+)\): (\S(?:.*\S)?)$/;
+                const headerPattern = /^(\S+)?: (\S+)-(\S+) (\S(?:.*\S)?)$/;
                 if (!headerPattern.test(parsed.header)) {
-                    throw new Error('❌ Invalid commit message format!(check for any extra whitespace character.) Expected format: "type-ticketKey-ticketNumber(moduleName): description');
+                    throw new Error('❌ Invalid commit message format!(check for any extra whitespace character.) Expected format: "type: ticketKey-ticketNumber description');
                 }
                 return [true];
             },
@@ -28,10 +28,10 @@ module.exports =  {
                 }
                 return [true];
             },
-            'ticket-key-format': (parsed, _when) => {
+            'ticket-key-format': (parsed, _when, expectedValues) => {
                 const { ticketKey } = parsed;
-                if(ticketKey.length > 7) {
-                    return [false, `❌ ticketKey must be less than 8 characters. Current length is ${ticketKey.length} characters`];
+                if(ticketKey.length > expectedValues) {
+                    return [false, `❌ ticketKey length must be atmost ${expectedValues} characters. The current length is ${ticketKey.length} characters`];
                 }
                 if(!parsed.ticketKey || !/^[A-Z]+$/.test(parsed.ticketKey)) {
                     return[false, '❌ ticketKey must be uppercase letters only.'];
@@ -45,27 +45,17 @@ module.exports =  {
                 }
                 return [true];
             },
-            'moduleName-format': (parsed, _when, expectedValues) => {
-                const { moduleName } = parsed;
-                if (!moduleName || !/^[a-z]+$/.test(moduleName)) {
-                    return [false, '❌ moduleName must be a valid string which contains lowercase alphabets only.'];
-                }
-                if (!moduleName || !expectedValues.includes(moduleName)) {
-                    return [false, `❌ moduleName must be one of ${expectedValues.join(', ')}`];
-                }
-                return [true];
-            },
             'description-min-length': (parsed, _when, expectedValues) => {
                 const { description } = parsed;
                 if (!description || description?.length < expectedValues) {
-                    return [false, `description must be at least ${expectedValues} characters, current length is ${description.length || 0}`];
+                    return [false, `Description must be at least ${expectedValues} characters. The current length is ${description.length || 0} characters`];
                 }
                 return [true];
             },
             'description-max-length': (parsed, _when, expectedValues) => {
                 const { description } = parsed;
                 if (!description || description?.length > expectedValues) {
-                    return [false, `description must be less than ${expectedValues} characters, current length is ${description.length || 0}`];
+                    return [false, `Description length must be at most ${expectedValues} characters. The current length is ${description.length || 0} characters`];
                 }
                 return [true];
             },
@@ -75,9 +65,8 @@ module.exports =  {
         'header-format': [2, 'always'],
         'type-empty': [2, 'never'],
         'type-enum': [2, 'always', ['feat', 'fix', 'chore', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'revert']],
-        'ticket-key-format': [2, 'always'],
+        'ticket-key-format': [2, 'always', 10],
         'ticket-number-format': [2, 'always'],
-        'moduleName-format': [2, 'always', ['admn', 'coll', 'cove', 'loan', 'reln', 'exce', 'expo', 'custp', 'file', 'flex', 'memo', 'onbd', 'rskr', 'sprd', 'dash', 'othr']],
         'description-min-length': [2, 'always', 10],
         'description-max-length': [2, 'always', 50],
     }
